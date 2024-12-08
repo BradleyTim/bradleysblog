34dc517a71c3a5f8d02412ea9024718e8d825cfc
BradleyTim <timbradley94@gmail.com>
2024-12-08 21:55:02 +0300

    update ui to display errors

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -7,14 +7,23 @@
                 <label for="title">A Title</label>
                 <input type="text" name="title" id="title" placeholder="Here is a sample title">
             </div>
+            @error('title')
+                <p class="error">{{ $message }}</p>
+            @enderror
             <div class="formgroup">
                 <label for="slug">Your Slug</label>
                 <input type="text" name="slug" id="slug" placeholder="A slug goes here">
             </div>
+            @error('slug')
+                <p class="error">{{ $message }}</p>
+            @enderror
             <div class="formgroup">
                 <label for="body">Blog Body</label> 
                 <textarea name="body" id="body" cols="30" rows="10" placeholder="What is on your mind? "></textarea>
             </div>
+            @error('body')
+                <p class="error">{{ $message }}</p>
+            @enderror
             <div>
                 <button type="submit">Save Blog</button>
             </div>
