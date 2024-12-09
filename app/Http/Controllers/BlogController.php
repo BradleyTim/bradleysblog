af9f7cadb92bc335ffa7ee1b2293a3cd43598da9
BradleyTim <timbradley94@gmail.com>
2024-12-09 21:43:06 +0300

    move actions to the controller and update routes to use controllers

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,8 +3,72 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('blogs.index', [
+            'blogs' => Blog::with('user')->latest()->simplePaginate(10)
+        ]);
+    }
+
+    public function show(Blog $blog)
+    {
+        return view('blogs.show', ['blog' => $blog]);
+    }
+
+    public function create()
+    {
+        return view('blogs.create');
+    }
+
+    public function store(Request $request)
+    {
+        request()->validate([
+            'title' => ['required', 'min:3', 'max:255'],
+            'slug' => ['required', 'min:3', 'max:255'],
+            'body' => ['required', 'min:3']
+        ]);
+    
+        Blog::create([
+            'title' => request('title'),
+            'slug' => request('slug'),
+            'body' => request('body'),
+            'user_id' => 1
+        ]);
+    
+        return redirect('/blog');
+    }
+
+    public function edit(Blog $blog)
+    {
+        return view('blogs.edit', ['blog' => $blog]);
+    }
+
+    public function update(Blog $blog)
+    {
+        //todo: authorize 
+
+        request()->validate([
+            'title' => ['required', 'min:3', 'max:255'],
+            'slug' => ['required', 'min:3', 'max:255'],
+            'body' => ['required', 'min:3']
+        ]);
+
+        $blog->update([
+            'title' => request('title'),
+            'slug' => request('slug'),
+            'body' => request('body'),
+        ]);
+
+        return redirect("/blog/{$blog->id}");
+    }
+
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+        return redirect('/blog');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Blog;
 
 Route::get('/', function () {
     return view('index', ['greeting' => 'Hello', 'name' => 'Bradley']);
@@ -15,68 +15,10 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/blog', function () {
-    return view('blogs.index', [
-        'blogs' => Blog::with('user')->latest()->simplePaginate(10)
-    ]);
-});
-
-Route::get('/blog/create', function () {
-    return view('blogs.create');
-});
-
-Route::get('/blog/{id}', function ($id) {
-    $blog = Blog::with('user')->findOrFail($id);
-    return view('blogs.show', ['blog' => $blog]);
-});
-
-Route::post('/blog', function () {
-    
-    request()->validate([
-        'title' => ['required', 'min:3', 'max:255'],
-        'slug' => ['required', 'min:3', 'max:255'],
-        'body' => ['required', 'min:3']
-    ]);
-
-    Blog::create([
-        'title' => request('title'),
-        'slug' => request('slug'),
-        'body' => request('body'),
-        'user_id' => 1
-    ]);
-
-    return redirect('/blog');
-});
-
-Route::get('/blog/{id}/edit', function ($id) {
-    $blog = Blog::with('user')->findOrFail($id);
-    return view('blogs.edit', ['blog' => $blog]);
-});
-
-Route::patch('/blog/{id}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'min:3', 'max:255'],
-        'slug' => ['required', 'min:3', 'max:255'],
-        'body' => ['required', 'min:3']
-    ]);
-
-    $blog = Blog::with('user')->findOrFail($id);
-
-    //todo: authorize
-
-    $blog->update([
-        'title' => request('title'),
-        'slug' => request('slug'),
-        'body' => request('body'),
-    ]);
-
-    return redirect("/blog/{$blog->id}");
-});
-
-Route::delete('/blog/{id}', function ($id) {
-    //todo: authorize
-
-    $blog = Blog::with('user')->findOrFail($id);
-    $blog->delete();
-    return redirect('/blog');
-});
+Route::get('/blog', [BlogController::class, 'index']);
+Route::post('/blog', [BlogController::class, 'store']);
+Route::get('/blog/create', [BlogController::class, 'create']);
+Route::get('/blog/{blog}', [BlogController::class, 'show']);
+Route::get('/blog/{blog}/edit', [BlogController::class, 'edit']);
+Route::patch('/blog/{blog}', [BlogController::class, 'update']);
+Route::delete('/blog/{blog}', [BlogController::class, 'destroy']);
