7c5ac78679ea6068646a3dd7865e350f145be1a7
BradleyTim <timbradley94@gmail.com>
2024-12-12 20:09:44 +0300

    update content on contact page and add styles

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
     <section class="container">
-        <h1>Contact - Bradley Tim</h1>
+        <h1>Contact Me</h1>
+        <div>
+            <p>Due to the number of messages I receive everyday, for my own mental sanity, I’ve decided to remove an easy way to contact me. My time is valuable to me, my family and my friends, and I just have no way to respond to all of the messages I receive.</p>
+            <p>If you’d like to hire me for consulting, you can book a paid call with me <a href="#">here.</a></p>
+            <p>You can always <a href="#">tweet at me</a>. However again, I can’t respond to every <a href="#">tweet.</a></p>
+        </div>
     </section>
 </x-layout>
