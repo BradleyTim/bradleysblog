1257e51df7357f776a4cc1ed6c22a317d37ed729
BradleyTim <timbradley94@gmail.com>
2025-01-07 20:35:38 +0300

    update index page copy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
     <section class="container">
-        <h1>{{$greeting}}, my name is {{$name}}. Welcome to my page.</h1>
+        <h1>{{$greeting}},</h1>
+        <p> I'm {{$name}}, a lawyer and Technologist living in Nairobi, Kenya.</p> 
+        <p>I currently work at Tourism Fund, developing a more sustainable and equitable Tourism Industry for Kenya. I've worked previously at Commission on Revenue Allocation, and Judiciary.</p>
+        <p>I graduated from Kenyatta University in 2017 with a degree in Law and from Kenya School of Revenue Athority in 2024 with a Post Graduate degree in Tax Administration.</p>
     </section>
 </x-layout>
