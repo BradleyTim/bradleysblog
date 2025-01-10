115a9fd3dfcabe1a007df90edc8f02cac4c4134f
BradleyTim <timbradley94@gmail.com>
2025-01-10 22:22:02 +0300

    update page ui

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,9 +1,11 @@
 <x-layout>
     <section class="container">
         <h1>Blogs</h1>
+        <p>Strong opinions and shared thoughts on law, business, and tech. Since 1999.</p>
         <div>
             @foreach($blogs as $blog)
             <article>
+                <p>{{ 'Bradley' }} | {{ $blog->created_at->toFormattedDateString() }}</p>
                 <a href="/blog/{{ $blog->id }}">
                     {{ $blog->title }}
                 </a>
